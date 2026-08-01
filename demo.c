c527b7ee9d4b09be139500bfaeeda3b2e08838ae
arshad <arshadpyaseen@gmail.com>
2026-08-01 18:58:09 +0530

    error propagation c emit

--- demo.c
+++ demo.c
@@ -2,40 +2,43 @@
 #include <stdint.h>
 
 typedef uint32_t nul_error;
+#define nul_error_Nice ((nul_error)1)
 
-static int64_t nul_count_up_0(int64_t t0);
+typedef struct nul_tag_19 nul_err_19;
+
+struct nul_tag_19 { nul_error err; int64_t value; };  /* !i64 */
+
+static nul_err_19 nul_count_up_0(int64_t t0);
 static int64_t nul_main_1(void);
 
 /* count_up */
-static int64_t nul_count_up_0(int64_t t0) {
-    int64_t* t2;
-    int64_t t2_slot;
-    int64_t t4;
-    bool t5;
-    int64_t t7;
-    int64_t t8;
-    int64_t t11;
-    t2 = &t2_slot;
-    *t2 = (int64_t)1;
-b1:;
-    t4 = *t2;
-    t5 = t4 < t0;
-    if (t5) goto b2; else goto b3;
-b2:;
-    t7 = *t2;
-    t8 = (int64_t)((uint64_t)t7 * (uint64_t)(int64_t)2);
-    *t2 = t8;
-    goto b1;
-b3:;
-    t11 = *t2;
-    return t11;
+static nul_err_19 nul_count_up_0(int64_t t0) {
+    nul_err_19 t2;
+    t2 = (nul_err_19){ .err = nul_error_Nice };
+    return t2;
 }
 
 /* main */
 static int64_t nul_main_1(void) {
-    int64_t t1;
+    nul_err_19 t1;
+    bool t2;
+    int64_t* t3;
+    int64_t t3_slot;
+    int64_t t4;
+    int64_t t7;
     t1 = nul_count_up_0((int64_t)100);
-    return t1;
+    t2 = (t1).err != 0;
+    t3 = &t3_slot;
+    if (t2) goto b2; else goto b1;
+b1:;
+    t4 = (t1).value;
+    *t3 = t4;
+    goto b3;
+b2:;
+    *t3 = (int64_t)0;
+b3:;
+    t7 = *t3;
+    return t7;
 }
 
 int main(void) { return (int)nul_main_1(); }
